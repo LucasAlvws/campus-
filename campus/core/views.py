c61f43b58eb612f99b1709a8c3bc97cfda76f12d
Lucas Alves <sociallucasalves@gmail.com>
2025-06-09 12:57:06 -0300

    feat: initial commit

---
+++ campus/core/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from campus.core.forms import SignUpForm
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.is_staff = True
+            user.is_superuser = True
+            user.save()
+            login(request, user)
+            return redirect('/admin/')
+    else:
+        form = SignUpForm()
+    return render(request, 'registration/signup.html', {'form': form})
+
+
---
+++ campus/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic.base import RedirectView
+from django.urls import path
+from campus.core import views as my_views
+
+urlpatterns = [
+    # redireciona o admin/login para o seu login customizado
+    path('admin/login/', RedirectView.as_view(url='/accounts/login/', permanent=False)),
+
+    path('accounts/', include('django.contrib.auth.urls')),  # login, logout, password reset etc.
+    path('accounts/signup/', my_views.signup_view, name='signup'),
+
+    path('admin/', admin.site.urls),
+
+    # sua URL raiz
+    path('', RedirectView.as_view(url='/admin/', permanent=False)),
+]
